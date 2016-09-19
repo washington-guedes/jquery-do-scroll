58e31e83ff7b06ce828f42174c181e2d1a442703
Washington Luís Guedes <wlg.96@hotmail.com>
2016-09-19 14:14:49 -0300

    Performance changes

--- jquery.scroll.custom.js
+++ jquery.scroll.custom.js
@@ -7,106 +7,132 @@ Number.prototype.between = function(min, max) {
 $.fn.scroll = function(obj) {
     obj = obj || {};
     var that = this[0];
-    var $that = this;
+    var $that = this.css('overflow', 'hidden');
     var width = $that.width();
     var height = $that.height();
     var offset = $that.offset();
+    var offTop = offset.top;
     var maxTop = -height;
     $that.children('img').each(function() {
         $(this).on('load', function() {
+            $(this).css({
+                position: 'relative',
+                float: 'left',
+            });
             maxTop += $(this).height();
         });
     });
-    var barHeight;
-    if (obj.bar) {
-        obj.bar = $(obj.bar).detach().appendTo('body');
-        barHeight = obj.bar.height();
-        obj.bar.css({
+    var thisBar = $(obj.bar);
+    if (thisBar.length) {
+        thisBar.detach().appendTo('body');
+        thisBar.css({
             position: 'absolute',
             left: offset.left + width,
-        }).show();
+        });
     }
     var elastic = obj.elastic;
     var getSpace = false;
-    var fnSpaceChanged;
-    var points = obj.arrayFnSpaces;
-    var barMaxTop = height - barHeight;
+    var barMaxTop = height - thisBar.height();
     var startAtTop;
     var startY = 0;
     var distance;
-    var lastMove;
+    var touchStarted;
+    var lastRun;
+    var elapsedTime = function(from, to, inSeconds) {
+        var milliseconds = (+(to || new Date)) - (+from);
+        return inSeconds ? milliseconds / 1000 : milliseconds;
+    };
     var lastSpace = 1;
     var currentSpace = 1;
     var getY = function(e) {
         if (isNaN(e.pageY)) {
             e = e.originalEvent;
             e = e.touches[0] || e.changedTouches[0];
-            return e.pageY;
         }
         return e.pageY;
     };
-    var fnDown = function(e) {
+    var prevent = function(e) {
+        if (typeof e.preventDefault === 'function') {
+            e.preventDefault();
+        }
         if (typeof e.stopPropagation === 'function') {
             e.stopPropagation();
         }
+        if (typeof e.stopImmediatePropagation === 'function') {
+            e.stopImmediatePropagation();
+        }
+    };
+    var fnDown = function(e) {
+        prevent(e);
         startY = getY(e);
         startAtTop = that.scrollTop;
+        touchStarted = new Date;
         $that.on(moveEvent, fnMove);
     };
-    var moveEvent = 'mousemove touchmove';
-    var fnMove = function(e) {
-        lastMove = new Date;
-        distance = startY - getY(e);
-        $that.scrollTop((startAtTop + distance).between(0, maxTop));
-        if (barHeight) {
-            obj.bar.css({
-                top: that.scrollTop / maxTop * barMaxTop + offset.top,
-            });
+    var points = obj.arrayFnSpaces || [function() {}];
+    var fnSpaceChanged = points[0];
+    points[0] = 0;
+    points.push(Infinity);
+    var totalPoints = points.length;
+    var getSpace = function(y) {
+        for (var i = 0; i < totalPoints; i++) {
+            if (y < points[i]) {
+                return i;
+            }
         }
-        if (getSpace !== false) {
+        return totalPoints;
+    };
+    var moveEvent = 'mousemove touchmove';
+    var running = false;
+    var fnMove = function(e) {
+        if (running) {
+            return;
+        }
+        running = true;
+        window.requestAnimationFrame(function() {
+            lastRun = new Date;
+            distance = startY - getY(e);
+            $that.scrollTop((startAtTop + distance).between(0, maxTop));
+            thisBar.css('top', that.scrollTop / maxTop * barMaxTop + offTop);
             currentSpace = getSpace(that.scrollTop);
             if (currentSpace !== lastSpace) {
-                fnSpaceChanged(currentSpace);
+                fnSpaceChanged(currentSpace, lastSpace);
                 lastSpace = currentSpace;
             }
-        }
+            running = false;
+        });
     };
-    fnMove({ pageY: 0 });
     var fnUp = function(e) {
-        var delay = distance / 3.5 | 0;
-        var delayTime = 20;
         $that.off(moveEvent, fnMove);
-        if (!elastic || +new Date - (+lastMove) > 200) return;
-        (function setDelay(counter, i) {
-            if (counter) {
-                setTimeout(function() {
-                    var newY = getY(e) - delay * i;
-                    fnMove({ pageY: newY });
-                    setDelay(counter - 1, i + 1);
-                }, delayTime);
-            }
-        })(Math.sqrt(Math.abs(delay)) | 0, 1);
-    };
-    if (typeof points === 'object' && points.map === [].map) {
-        var fnSpaceChanged = points[0];
-        if (points.length && typeof fnSpaceChanged === 'function') {
-            points[0] = 0;
-            points.push(Infinity);
-            getSpace = function(y) {
-                var l = points.length;
-                for (var i = 0; i < l; i++) {
-                    if (y < points[i]) {
-                        return i;
-                    }
-                }
-                return l;
-            };
+        if (elastic && elapsedTime(lastRun) < 200) {
+            var totalTime = elapsedTime(touchStarted, 1);
+            newTime = totalTime.between(2, totalTime);
+            elasticEffect(getY(e), distance / newTime, 20);
         }
-    }
-    $that.on('mousedown touchstart', fnDown).on('mouseup touchend', fnUp);
+    };
+    var elasticEffect = function(y, slip, delay) {
+        var elasticStarted = new Date;
+        var counter = Math.sqrt(Math.abs(slip) | 0) | 0;
+        var slipX = slip / counter;
+        (function effect(counter, i) {
+            var scrollEnded = elapsedTime(touchStarted, elasticStarted) < 0;
+            if (!counter || scrollEnded) {
+                return;
+            }
+            setTimeout(function() {
+                fnMove({ pageY: y - slipX * i });
+                effect(counter - 1, i + 1);
+            }, delay);
+        })(counter, 1);
+    };
+    $that.on('mousedown touchstart', fnDown)
+        .on('mouseup touchend', fnUp)
+        .on('scroll mousewheel', prevent);
+    fnMove({ pageY: 0 });
+    thisBar.show();
     return {
         moveToSpace: function(space) {
-            fnDown({ pageY: 0 })
+            fnDown({ pageY: 0 });
             var y = that.scrollTop - points[space - 1];
             fnMove({ pageY: y });
             var isElastic = elastic;
